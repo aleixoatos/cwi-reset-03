48a7daec630f19041777d8d0ba326f8d4b8d83aa
Alexandre Oliveira <alexandreatos@hotmail.com>
2021-10-07 10:57:51 -0300

    Alteração do valor do atributo torta, da classe ItemPorQuantidade, considerando que as vendas são por fatias. Correção da String pão, no if da classe ReposicaoCozinha.

--- desafio-aula-1/registradora-cheia-de-bugs/src/ReposicaoCozinha.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/ReposicaoCozinha.java
@@ -1,11 +1,11 @@
 public class ReposicaoCozinha {
 
     static void reporItem(String item) {
-        if ("paes".equals(item)) {
+        if ("pao".equals(item)) {
             ItensPorQuantidade.pao += 3600;
         }
         if ("torta".equals(item)) {
-            ItensPorQuantidade.torta += 4;
+            ItensPorQuantidade.torta += 64;
         }
         if ("sanduiche".equals(item)) {
             ItensPorQuantidade.sanduiche += 20;

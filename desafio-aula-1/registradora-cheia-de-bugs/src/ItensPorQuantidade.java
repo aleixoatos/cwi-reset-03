5e6bc196d88b4e51ad86348d4e82a96a7a6eb306
Alexandre Oliveira <alexandreatos@hotmail.com>
2021-10-07 09:53:01 -0300

    Criação do método atualizaQuantidade, na classe ItensPorQuantidade. Este método pega a quantidade atualizada de cada item do estoque, e mostra ao atendente todas as vezes que a cozinha estiver fechada e não puder haver a reposição dos itens.

--- desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -6,5 +6,24 @@ public class ItensPorQuantidade {
     static int leite = 20;
     static int cafe = 20;
 
+    static int atualizaQuantidade(String item) {
+        if ("pao".equals(item)) {
+            return pao / 60;
+        }
+        if ("torta".equals(item)) {
+            return torta / 16;
+        }
+        if ("sanduiche".equals(item)) {
+            return sanduiche;
+        }
+        if ("leite".equals(item)) {
+            return leite;
+        }
+        if ("cafe".equals(item)) {
+            return cafe;
+        }
+        return 0;
+
+    }
 
 }

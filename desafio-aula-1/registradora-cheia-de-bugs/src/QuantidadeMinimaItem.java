1e20eae73000d3af9bd9f05b3199e08baa148d5e
Alexandre Oliveira <alexandreatos@hotmail.com>
2021-10-07 11:32:17 -0300

    Criação do método checaReposicao, na Classe QuantidadeMinimaItem. A ideia é fazer com que este método verifique qual é o item, se necessita de reposição, se a cozinha está em funcionamento e informa ao atendente o que ele precisa saber: se precisa de reposição, se a cozinha está fechada e qual a quantidade atualizada de estoque. Se a cozinha estiver funcionando, o método também já faz a reposição do itens que estão abaixo do limite.

--- desafio-aula-1/registradora-cheia-de-bugs/src/QuantidadeMinimaItem.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/QuantidadeMinimaItem.java
@@ -23,4 +23,21 @@ public class QuantidadeMinimaItem {
 
         return false;
     }
+    static void checaReposicao (String item) {
+        if (("pao".equals(item) || "sanduiche".equals(item) || "torta".equals(item))) {
+            if (precisaReposicao(item) && DataProjeto.cozinhaEmFuncionamento()) {
+                ReposicaoCozinha.reporItem(item);
+                System.out.println("O Estoque de " + item + " está baixo e é necessário que sejam preparados mais " + item + "s para reposição!");
+            }
+            if (precisaReposicao(item) && !DataProjeto.cozinhaEmFuncionamento()) {
+                System.out.println("Cozinha fechada! No momento o item " + item + " não está disponível para reposição. \nRestam "+ ItensPorQuantidade.atualizaQuantidade(item) + " unidades disponíveis em estoque!");
+            }
+        }
+        if (("leite".equals(item) || "cafe".equals(item))) {
+            if (precisaReposicao(item)) {
+                ReposicaoFornecedor.reporItem(item);
+                System.out.println("O Estoque de " + item + " está baixo, e é necessário que sejam preparados mais " + item + "s para reposição!");
+            }
+        }
+    }
 }

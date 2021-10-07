c5cf160640a0f033114a17189e81f157f8bc8903
Alexandre Oliveira <alexandreatos@hotmail.com>
2021-10-07 09:39:57 -0300

    Criação do método liberaPedido, dentro da Classe EstoqueAtual. Este método testa se a quantidade de itens solicitados pelos clientes é maior que a quantidade disponível no estoque, na classe ItensPorQuantidade. Se a quantidade pedida for superior ao que tem disponível em estoque, mas com a cozinha aberta, o sistema libera a venda, se a cozinha estiver fechada, o sistema bloqueia o pedido.

--- desafio-aula-1/registradora-cheia-de-bugs/src/EstoqueAtual.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/EstoqueAtual.java
@@ -17,4 +17,27 @@ public class EstoqueAtual {
             ItensPorQuantidade.cafe -= quantidade;
         }
     }
+    static boolean liberaPedido (String item, int quantidade) {
+        if ("pao".equals(item) && quantidade > ItensPorQuantidade.pao / 60) {
+            return false;
+        }
+
+        if ("torta".equals(item) && quantidade > ItensPorQuantidade.torta) {
+            return false;
+        }
+
+        if ("sanduiche".equals(item) && quantidade > ItensPorQuantidade.sanduiche) {
+            return false;
+        }
+
+        if ("leite".equals(item) && quantidade > ItensPorQuantidade.leite) {
+            return false;
+        }
+
+        if ("cafe".equals(item) && quantidade > ItensPorQuantidade.cafe) {
+            return false;
+        }
+
+        return true;
+    }
 }
